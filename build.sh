bce3aa6d96c29e646c8e0269260a1c9992116131
Masaru Nagaku <mnagaku@gmail.com>
2015-12-17 10:52:22 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker run --rm -v ".:/src" mnagaku/jnaerator:0.9.2 java -jar jnaerator.jar -genCPlusPlus /src/HelloWorld.h
-
 cd ..
+
+docker run --rm -v "$PWD/docker-jna-sample:/src" mnagaku/jnaerator:0.9.2 java -jar jnaerator.jar -genCPlusPlus /src/HelloWorld.h
+
 docker build -t docker-jna-sample -f docker-jna-sample/Dockerfile .
+
